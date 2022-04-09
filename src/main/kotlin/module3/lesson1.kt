47d8aba4d9c1f58aad6b520e36e96df5bc9d7d39
ZorigtoDeveloper <zorigto@badmain.ru>
2022-04-09 14:40:58 +0900

    studied lambda expressions and unit functions

--- src/main/kotlin/module3/lesson1.kt
+++ src/main/kotlin/module3/lesson1.kt
@@ -21,15 +21,30 @@ fun main() {
     calculateSavings(middleGrossSalary, 0.25)
     calculateSavings(seniorGrossSalary)
 
-    val sum = calculateSavings(30000, 0.12, 8) +
-            calculateSavings(55000, 0.20, 8)
-    println(sum)
+    // val sum = calculateSavings(30000, 0.12, 8) +
+           // calculateSavings(55000, 0.20, 8)
+
+    val first = calculateSavings(30000, 0.12, 8) { _, _, _, bank ->
+        println("First member's bank: $bank")
+    }
+    val second = calculateSavings(55000, 0.20, 8, printInfo = {salary, rate, period, bank ->
+        println("Second member's bank: $bank")
+    })
+    val third = calculateSavings(50000, 0.30, 12, printInfo = {salary, rate, period, bank ->
+        println("Salary: $salary, rate: ${rate * 100}%, period: $period months, $bank in 1 year")
+    })
+    println(first + second)
 }
 
-fun calculateSavings(salary: Int, rate: Double = 0.3, period: Int = 12) : Double {
+fun calculateSavings(salary: Int,
+                     rate: Double = 0.3,
+                     period: Int = 12,
+                     printInfo: ((salary: Int, rate: Double, period: Int, bank: Double) -> Unit)? = null
+) : Double {
     val netSalary = salary * 0.87
     val bank = netSalary * rate * period
-    println("Salary: $salary, rate: ${rate * 100}%, period: $period months, $bank in 1 year")
+    printInfo?.invoke(salary,rate, period, bank)
+   // println("Salary: $salary, rate: ${rate * 100}%, period: $period months, $bank in 1 year")
     return  bank
 }
 
